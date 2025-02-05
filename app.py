de89e6e10f8d733d4dcf5d6345e460c83c227776
Paulo Cesar Mota Leonardo <paulocesarml85@gmail.com>
2025-02-04 22:07:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,6 @@ from langchain.vectorstores import Chroma
 from pypdf.errors import PdfReadError
 from openai.error import AuthenticationError, InvalidRequestError
 
-# Adicionar a imagem no cabeçalho
-image_url = "https://cienciadosdados.com/images/CINCIA_DOS_DADOS_4.png"
-st.image(image_url, use_column_width=True)
-
 # Adicionar o nome do aplicativo
 st.subheader("Q&A com IA - PLN usando LangChain")
 
